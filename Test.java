1e7c8b41fff519965500b5a2e2fd6c13383496b1
sueelpunnana <suneelpunnana3@gmail.com>
2019-09-30 14:23:34 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+	public void test() {
+		add a=new add();
+		assertEquals(12,a.Addf(8,4));
+
+	}
+}
---
+++ add.java
@@ -0,0 +1,4 @@
+public class add{
+public int add(int x,int y){
+return x+y;
+}
